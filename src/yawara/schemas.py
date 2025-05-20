184bc80f943b0223c38468df3ca1edbcc6e5ff8a
Rafael <rafaelgonzag4@gmail.com>
2025-05-20 08:57:15 -0300

    Modificação dos schemas

--- src/yawara/schemas.py
+++ src/yawara/schemas.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from typing import List, Optional
 from pydantic import BaseModel, ConfigDict, EmailStr
 
 from yawara.models import Tipo
@@ -9,6 +11,48 @@ class Token(BaseModel):
 
 class Message(BaseModel):
     message: str
+
+class UsuarioBase(BaseModel):
+    login: str
+
+    class Config:
+        orm_mode = True
+
+class FuncionarioBase(BaseModel):
+    id: int
+    nome: str
+    cpf: str
+    email: str
+    tipo: Tipo
+    usuario: UsuarioBase  # Aqui acessamos apenas o login
+
+    class Config:
+        orm_mode = True
+
+class PetBase(BaseModel):
+    id: int
+    nome: str
+    class Config:
+        orm_mode = True
+
+class ClienteBase(BaseModel):
+    id: int
+    nome: str
+    cpf: str
+    email: str
+    telefone: str
+    usuario: UsuarioBase  # Aqui acessamos apenas o login
+
+class ClienteBaseComPets(BaseModel):
+    id: int
+    nome: str
+    cpf: str
+    email: str
+    telefone: str
+    usuario: UsuarioBase  # Aqui acessamos apenas o login
+    pets: List[PetBase]
+    class Config:
+        orm_mode = True
 
 class UsuarioFuncionarioSchema(BaseModel):
     login: str
@@ -22,5 +66,36 @@ class UsuarioFuncionarioSchema(BaseModel):
 class UsuarioFuncionarioPublic(BaseModel):
     id: int
     nome: str
+    cpf: str
     email: EmailStr
+    tipo: Tipo
+
+class UsuarioFuncionarioList(BaseModel):
+    usuarios: list[UsuarioFuncionarioPublic]
+
+class PetsSchema(BaseModel):
+    nome: str
+    dono: int
+
+class PetsList(BaseModel):
+    pets: list[PetsSchema]
+
+class UsuarioClienteSchema(BaseModel):
+    login: str
+    senha: str
+    nome: str
+    cpf: str
+    email: EmailStr
+    endereco: str
+    telefone: str
+
+class UsuarioClientePublic(BaseModel):
+    id: int
+    nome: str
+    email: EmailStr
+    pets: List[PetBase]
     model_config = ConfigDict(from_attributes=True)
+
+
+
+
